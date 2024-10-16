18e23bf50df5c5733f33f06990596f6a83b8f89d
Admin <macik9157@gmail.com>
2024-10-16 09:39:11 +0300

    умовні оперетори калькулятор

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 num_one = int(input("Enter a number one: "))
 num_two = int(input("Enter a number two: "))
-num_oper = str(input("Enter a number operation '+' or '-' : "))
+num_oper = str(input("Enter a number operation '+' or '-' or '*' or '/':' "))
 
 if num_oper == '+' :
     res = num_one + num_two
     print(res)
 elif num_oper == '-' :
     res = num_one - num_two
+    print(res)
+elif num_oper == '*' :
+    res = num_one * num_two
+    print(res)
+elif num_oper == '/' :
+    res  = int (num_one / num_two)
     print(res)
 else:
     print("Eror")
