c076cfc0afba76f831ee839513f160967ea4c2d5
Admin <macik9157@gmail.com>
2024-10-16 14:01:30 +0300

    умовні оперетори task 3/2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-num_one = int(input("Enter a number one: "))
+'''num_one = int(input("Enter a number one: "))
 num_two = int(input("Enter a number two: "))
 num_oper = str(input("Enter a number operation '+' or '-' or '*' or '/':' "))
 
@@ -15,4 +15,12 @@ elif num_oper == '/' and num_two !=0 :
     res  = int (num_one / num_two)
     print(res)
 else:
-    print("Eror")
+    print("Eror") '''
+
+#tast 3.2
+num =  [12 , 3 , 4 , 10]
+if len(num) > 1:
+    num_last = num.pop()
+    num.insert( 0 , num_last)
+print(num)
+
