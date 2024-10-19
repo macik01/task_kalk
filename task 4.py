7f55d4de5aefde12a10603e5ec61c33fe003776f
Admin <macik9157@gmail.com>
2024-10-19 10:16:30 +0300

    task 4.1 and 4.2 and 4/3

--- task 4.py
+++ task 4.py
@@ -8,9 +8,19 @@ while i < len(a):
     else:
         i += 1
 print(a)'''
+from random import random
+
 # task 4.2
-b = [0, 1, 7, 2, 4, 8]
+import random
+'''b = [0, 1, 7, 2, 4, 8]
 sum_b = sum(b[i] for i in range(0, len(b),2))
 print(sum_b)
 res = sum_b * b[-1]
-print(res)
+print(res)'''
+#task 4.3
+sp = [random.randint(1, 10) for _ in range(random.randint(3, 10))]
+print(sp)
+new_sp = [sp[0], sp[2] , sp[-2]]
+print(new_sp)
+sp_sort=sorted(sp)
+print(sp_sort)
