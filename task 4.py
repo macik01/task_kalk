0627a09fbc8abfcc3646380656309ea4ae49c229
Admin <macik9157@gmail.com>
2024-10-19 09:31:59 +0300

    task 4.1 and 4.2

--- task 4.py
+++ task 4.py
@@ -1,5 +1,5 @@
 #task 4.1
-i = 0
+''' i = 0
 a = [0, 1, 0, 12, 3]
 while i < len(a):
     if a[i] == 0:
@@ -7,4 +7,10 @@ while i < len(a):
         i +=1
     else:
         i += 1
-print(a)
+print(a)'''
+# task 4.2
+b = [0, 1, 7, 2, 4, 8]
+sum_b = sum(b[i] for i in range(0, len(b),2))
+print(sum_b)
+res = sum_b * b[-1]
+print(res)
